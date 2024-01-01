fcf718ed109fc68b840bfa5f2b0ab8dd6d2cd5bf
Humayun-Moon <humayunfreelancher01@gmail.com>
2024-01-01 08:26:49 +0600

    BroCode

---
+++ function/main.py
@@ -0,0 +1,31 @@
+# function = A block of reusable code  
+# Place () after the function name to invoke it 
+
+
+# print("Happy birthday to Humayun")
+# print("You are now 24 years old")
+# print("Have a nice year")
+# print()
+
+# print("Happy birthday to Humayun")
+# print("You are now 24 years old")
+# print("Have a nice year")
+# print()
+
+
+
+# def birthday (name, plan,age):
+
+#     print(f"Happy birthday to {name} {plan} remaind it already you have reached at {age} ")
+    
+#     print() 
+# birthday("Humayun","What's your  plan will be in this year", 24)    
+# birthday("Moon","What's your  plan will be in this year", 19)    
+# birthday("Basar","What's your  plan will be in this year", 16)    
+# birthday("Abdur Rhaman","What's your  plan will be in this year", 15)    
+
+
+def display_invoice (uesr, amount, due_date):
+    print(f"Hello,  {uesr:.10}")
+    print(f"Your total bill of {amount:.2f} due to {due_date}")
+display_invoice("Humayun", 20221, "20-01-2024")    
